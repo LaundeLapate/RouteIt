0abba34384d84ab5850b1d0353c691374f21a2c0
vivonk <nirmalsarswat400@gmail.com>
2020-01-06 04:53:58 +0530

    log config added

--- pkg/const.go
+++ pkg/const.go
@@ -3,3 +3,6 @@ package pkg
 var ApiServerPort = "80"
 var PingServerPort = "9413"
 var Host = "0.0.0.0"
+var LoggingDir = "/var/log/routeIt/" // NOTE :: change it in dev window environment
+var RootLoggingFileName = "route_it_logs.log"
+var SysManagerLoggingFileName = "sys_manager_logs.log"
