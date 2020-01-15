2fa71f0d1d0fc880df96f7a98429e2de8e392755
abhishek20123g <abhishek20123g@gmail.com>
2020-01-15 22:24:12 +0530

    Adding global cache files

---
+++ pkg/services/cache/globalcache.go
@@ -0,0 +1,7 @@
+/*
+This module consist of cache implementation of
+variable and datatype which will be require both
+by Routing Server and WebApplication and therefore
+can't be their in any module.(To avoid cyclic dependency)
+ */
+package cache
